4ce0e852257a536a4e21130a661e9d3d2d49e01a
Qingyi Chang <esmidth@163.com>
2019-07-19 20:17:01 +0800

    Test Vue.js

---
+++ frontend/src/components/HelloVUE.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="hellopp">
+    <h2>{{author}}</h2>
+    <h3>Hlfjaljdflaj</h3>
+  </div>
+
+</template>
+
+<script>
+  export default {
+    name: "HelloVUE",
+    data() {
+      return {
+        author: 'Esmidth'
+      }
+    }
+
+  }
+</script>
+
+<style scoped>
+
+</style>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import HelloWorld from '../components/HelloWorld'
+import HelloVUE from '../components/HelloVUE'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/p2',
+      name: 'HelloVue',
+      component: HelloVUE
     }
   ]
 })
